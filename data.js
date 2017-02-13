d33a42ce6636f88280059036fdd31c4a53e8689d
suayip ekmekci <suayipekmekci@users.noreply.github.com>
2017-02-13 12:41:07 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
 {
-    list:[
-    { value: 'lp', label: 'Than last period' },
-    { value: 'lw', label: 'Than last week' },
-    { value: 'lm', label: 'Than last month' },
-    { value: 'ly', label: 'Than last year' }
+    "list":[
+    { "value": "lp", "label": "Than last period" },
+    { "value": "lw", "label": "Than last week" },
+    { "value": "lm", "label": "Than last month" },
+    { "value": "ly", "label": "Than last year" }
   ],
-  iconName:"fa fa-twitter",
-  name:"Tweets",
-  value:"3,580",
-  trendIcon:"fa fa-chevron-circle-up",
-  trendValue:"23.3"
+  "iconName":"fa fa-twitter",
+  "name":"Tweets",
+  "value":"3,580",
+  "trendIcon":"fa fa-chevron-circle-up",
+  "trendValue":"23.3"
 }
